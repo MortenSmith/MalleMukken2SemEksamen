8bb2be76df18773d023d4fd049d40e64345fc682
Martin <10461942@ucn.dk>
2026-06-04 11:57:16 +0200

    ,,

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,23 @@
 // Navbar 
 function myFunction() {
-    const links = document.getElementById("myLinks");
-    links.classList.toggle("open");
+    if (window.innerWidth < 768) {
+        const links = document.getElementById("myLinks");
+        links.classList.toggle("open");
+    }
+}
+
+// Active class underline 
+document.querySelectorAll('#myLinks a').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('#myLinks a').forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+        sessionStorage.setItem('activeLink', this.getAttribute('href'));
+    });
+});
+
+// Gendan active class når siden loader
+const activeLink = sessionStorage.getItem('activeLink');
+if (activeLink) {
+    const link = document.querySelector(`#myLinks a[href="${activeLink}"]`);
+    if (link) link.classList.add('active');
 }
