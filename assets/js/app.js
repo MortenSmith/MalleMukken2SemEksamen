f6818108f8e102188049394fe34bc837a91f6211
Martin <10461942@ucn.dk>
2026-06-07 16:12:10 +0200

    YEIIHAAA FÆRDIG. måske 💨

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,3 +51,88 @@ document.addEventListener("click", function(e) {
         });
     }
 });
+
+// slideshow
+let slideIndex = 0;
+const slides = document.querySelectorAll(".myslides");
+const progressBar = document.getElementById("progressBar");
+
+function showSlide(n) {
+    slides.forEach(s => s.classList.remove("active"));
+    slideIndex = (n + slides.length) % slides.length;
+    slides[slideIndex].classList.add("active");
+    progressBar.style.width = ((slideIndex + 1) / slides.length * 100) + "%";
+}
+
+function changeSlide(n) {
+    showSlide(slideIndex + n);
+}
+
+if (window.innerWidth < 768) {
+    showSlide(0);
+    setInterval(() => changeSlide(1), 4000);
+} else {
+    slides.forEach(s => s.classList.add("active"));
+}
+
+// Start på første slide
+// showSlide(0);
+
+// Auto-skift hvert 4. sekund
+// setInterval(() => changeSlide(1), 10000);
+
+
+// slideshow for oplevelsecard
+// Oplevelse slideshow
+let oplevelseIndex = 0;
+const oplevelseCards = document.querySelectorAll(".oplevelse-card");
+const dots = document.querySelectorAll(".dot");
+
+function showOplevelseSlide(n) {
+    oplevelseCards.forEach(c => c.classList.remove("active"));
+    dots.forEach(d => d.classList.remove("active-dot"));
+    oplevelseIndex = (n + oplevelseCards.length) % oplevelseCards.length;
+    oplevelseCards[oplevelseIndex].classList.add("active");
+    dots[oplevelseIndex].classList.add("active-dot");
+}
+
+function currentslide(n) {
+    showOplevelseSlide(n - 1);
+}
+
+if (window.innerWidth < 768) {
+    showOplevelseSlide(0);
+    setInterval(() => showOplevelseSlide(oplevelseIndex + 1), 5000);
+} else {
+    oplevelseCards.forEach(c => c.classList.add("active"));
+}
+
+// Auto-skift hvert 5. sekund
+setInterval(() => showOplevelseSlide(oplevelseIndex + 1), 8000);
+
+// Start
+showOplevelseSlide(0);
+
+
+// Til top knap https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Hent id'et
+let mytopbutton = document.getElementById("mytopbtn");
+
+// Når brugeren scroller ned med 20px fra toppen, vis knappen
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const scrolledToButton = (window.innerHeight + window.scrollY) >= document.body.offsetHeight - 2000;
+
+    if (scrolledToButton) {
+        mytopbutton.style.display = "block"
+    } else {
+        mytopbutton.style.display = "none";
+    }
+}
+
+// Når brugeren klikker på knappen, gå til toppen
+function topFunction() {
+    document.body.scrollTop = 0; // til Safari
+    document.documentElement.scrollTop = 0; // til Chrome, firefox og Opera
+}
